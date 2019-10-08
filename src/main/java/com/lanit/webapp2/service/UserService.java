3d33f60dfd63f2950c46c27ec589121a3e8baf76
Dmitry <dmitry2500u@bk.ru>
2019-10-08 11:03:59 +0300

    Сервис-синглтон

--- src/main/java/com/lanit/webapp2/service/UserService.java
+++ src/main/java/com/lanit/webapp2/service/UserService.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
-    protected static UserService instance = new UserService();
+    protected static final UserService instance = new UserService();
 
     protected UserService() {}
 
